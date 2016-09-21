e36b4dfe761009fc0c215826a312d9ed14e1269f
Jan Hohmann <jhohmann@d120.de>
2016-09-21 23:52:15 +0200

    Migrated to LDAP

--- birthday.py
+++ birthday.py
@@ -6,9 +6,23 @@ from email.mime.text import MIMEText
 from configparser import ConfigParser
 from email.utils import formatdate
 
-def mail(addr, person):
-    config = ConfigParser()
-    config.read('config')
+from ldap3 import Server, Connection, ALL_ATTRIBUTES, SEARCH_SCOPE_WHOLE_SUBTREE
+
+LDAP_URL = "ldap://10.162.32.201"
+LDAP_USER_SCOPE = 'ou=People,dc=fachschaft,dc=informatik,dc=tu-darmstadt,dc=de'
+
+def init_ldap(config):
+    # Connect to LDAP
+    curUser = config['mailserver']['user'];
+    passwd = config['mailserver']['password'];
+    s = Server(LDAP_URL, use_ssl=True, get_info=ALL_ATTRIBUTES)
+    c = Connection(s, user="cn=" + curUser + ",ou=Services,dc=fachschaft,dc=informatik,dc=tu-darmstadt,dc=de", password=passwd)
+    if not c.bind():
+        print('Error in bind: ', c.result['description'])
+        exit()
+    return c
+
+def mail(addr, person, config):
     text = ""
     with open('birthdaytext', 'r') as f:
         text = f.read()
@@ -27,28 +41,26 @@ def mail(addr, person):
     s.quit()
 
 def main():
+    config = ConfigParser()
+    config.read('config')
+    c = init_ldap(config);
     today = date.today()
-    matcher = re.compile(r'\t')
-    if len(argv) != 2:
-        exit(1)
-    path = argv[1]
+    c.search(search_base=LDAP_USER_SCOPE, search_filter='(objectClass=d120Person)', search_scope=SEARCH_SCOPE_WHOLE_SUBTREE, attributes=['givenName', 'sn', 'mail', 'birthday', 'birthmonth'])
     addresses = []
     birthdays = []
-    with open(path, "r") as f:
-        for l in f:
-            if l[0] == '#':
-                continue
-            parsed = matcher.split(l)
-            if len(parsed) == 3:
-                addresses.append([parsed[0], parsed[2]])
-                birthday = datetime.strptime(parsed[1], '%d.%m.').replace(year=today.year).date()
-                delta = today - birthday
-                if delta.days == -1:
-                    birthdays.append(parsed[0])
+    for l in c.response:
+        attr = l['attributes']
+        if not 'birthday' in attr or not 'birthmonth' in attr:
+            continue
+        addresses.append([attr['givenName'][0],attr['mail'][0]])
+        birthday = date(today.year, int(attr['birthmonth'][0]), int(attr['birthday'][0]))
+        delta = today - birthday
+        if delta.days == -1:
+            birthdays.append(attr['givenName'][0] + ' ' + attr['sn'][0])
 
     if len(birthdays) > 0:
         for pers in birthdays:
-            mail(addresses, pers)
+            mail(addresses, pers, config)
 
 if __name__ == "__main__":
     main()
