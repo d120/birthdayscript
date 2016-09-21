f44cf8d2822dbef3c50a050c979c285838cbd01e
Heiko Carrasco <heiko.carrasco@fachschaft.informatik.tu-darmstadt.de>
2016-09-21 17:07:47 +0200

    Removed parentheses in config file

--- birthday.py
+++ birthday.py
@@ -15,36 +15,38 @@ def mail(addr, person):
     msg['Subject'] = config['mail']['subject']
     msg['From'] = config['mail']['from']
     msg['To'] = addr[1]
-    s = smtplib.SMTP('mail.d120.de', 587)
+    print(config['mailserver']['address'])
+    s = smtplib.SMTP(config['mailserver']['address'], 587)
     s.starttls()
     s.login(config['mailserver']['user'], config['mailserver']['password'])
     s.send_message(msg)
     s.quit()
 
+def main():
+    today = date.today()
+    matcher = re.compile(r'\t')
+    if len(argv) != 2:
+        exit(1)
+    path = argv[1]
+    addresses = []
+    birthdays = []
+    with open(path, "r") as f:
+        for l in f:
+            if l[0] == '#':
+                continue
+            parsed = matcher.split(l)
+            if len(parsed) == 3:
+                addresses.append([parsed[0], parsed[2]])
+                birthday = datetime.strptime(parsed[1], '%d.%m.').replace(year=today.year).date()
+                delta = today - birthday
+                if delta.days == -1:
+                    print("Happy Birthday %s" % parsed[0])
+                    birthdays.append(parsed[0])
 
-today = date.today()
-matcher = re.compile(r'\t')
-if len(argv) != 2:
-    exit(1)
-path = argv[1]
-addresses = []
-birthdays = []
-with open(path, "r") as f:
-    for l in f:
-        if l[0] == '#':
-            continue
-        parsed = matcher.split(l)
-        if len(parsed) == 3:
-            addresses.append([parsed[0], parsed[2]])
-            birthday = datetime.strptime(parsed[1], '%d.%m.').replace(year=today.year).date()
-            delta = today - birthday
-            print(delta)
-            if delta.days == -1:
-                print("Happy Birthday %s" % parsed[0])
-                birthdays.append(parsed[0])
+    if len(birthdays) > 0:
+        for pers in birthdays:
+            for address in addresses:
+                mail(address, pers)
 
-if len(birthdays) > 0:
-    for pers in birthdays:
-        for address in addresses:
-            mail(address, pers)
-
+if __name__ == "__main__":
+    main()
