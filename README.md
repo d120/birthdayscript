0618a4c63cb8a577be12e9eea8fc42d6e080dd3d
Heiko Carrasco <heiko.carrasco@fachschaft.informatik.tu-darmstadt.de>
2016-09-21 20:25:56 +0200

    Made birthdaytext a file which contents get send to the recpients

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Um das Skript zu starten muss die config.default in config umbenannt werden.
 
 Der Aufruf ist dann python3 birthday.py <Pfad zur birthday Datei>
+In der Datei birthdaytext wird der Text für das Anschreiben konfiguriert. Hierbei werden die Wörter RECEPIENT durch den Adressaten der E-Mail und
+BIRTHDAYKID durch den Geburtstagshabenden ersetzt.
--- birthday.py
+++ birthday.py
@@ -4,22 +4,26 @@ from datetime import datetime, date
 from sys import argv, exit
 from email.mime.text import MIMEText
 from configparser import ConfigParser
+from email.utils import formatdate
 
 def mail(addr, person):
     config = ConfigParser()
     config.read('config')
-    text = config['mail']['text']
-    text = text.replace('RECEPIENT', addr[0])
-    text = text.replace('BIRTHDAYKID', person)
-    msg = MIMEText(config['mail']['text'])
-    msg['Subject'] = config['mail']['subject']
-    msg['From'] = config['mail']['from']
-    msg['To'] = addr[1]
-    print(config['mailserver']['address'])
+    text = ""
+    with open('birthdaytext', 'r') as f:
+        text = f.read()
     s = smtplib.SMTP(config['mailserver']['address'], 587)
     s.starttls()
     s.login(config['mailserver']['user'], config['mailserver']['password'])
-    s.send_message(msg)
+    for address in addr:
+        text = text.replace('RECEPIENT', address[0])
+        text = text.replace('BIRTHDAYKID', person)
+        msg = MIMEText(text)
+        msg['Subject'] = config['mail']['subject']
+        msg['From'] = config['mail']['from']
+        msg['Date'] = formatdate()
+        msg['To'] = address[1]
+        s.send_message(msg)
     s.quit()
 
 def main():
@@ -40,13 +44,11 @@ def main():
                 birthday = datetime.strptime(parsed[1], '%d.%m.').replace(year=today.year).date()
                 delta = today - birthday
                 if delta.days == -1:
-                    print("Happy Birthday %s" % parsed[0])
                     birthdays.append(parsed[0])
 
     if len(birthdays) > 0:
         for pers in birthdays:
-            for address in addresses:
-                mail(address, pers)
+            mail(addresses, pers)
 
 if __name__ == "__main__":
     main()
